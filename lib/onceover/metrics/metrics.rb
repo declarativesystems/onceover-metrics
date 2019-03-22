9ed6cc0676e58772f20ccbac73f287621a5fe2a2
Geoff Williams <geoff@geoffwilliams.me.uk>
2019-03-22 14:18:44 +1100

    output almost there

--- lib/onceover/metrics/metrics.rb
+++ lib/onceover/metrics/metrics.rb
@@ -1,11 +1,29 @@
+require 'yaml'
+require 'csv'
+require 'deep_merge'
+
 class Onceover
   module Metrics
     module Metrics
-      PUPPETFILE        = "Puppetfile"
-      ENVIRONMENT_CONF  = "environment.conf"
-      TOTALS_KEY        = :totals
-      PUPPETFILE_KEY    = :puppetfile
-      SITE_MODULES_KEY  = :site_modules
+      PUPPETFILE            = "Puppetfile"
+      ENVIRONMENT_CONF      = "environment.conf"
+      TOTALS_KEY            = :totals
+      PUPPETFILE_KEY        = :puppetfile
+      SITE_MODULES_KEY      = :site_modules
+      HIERA_KEY             = :hiera
+      YAML_KEY              = :yaml
+      FILES_KEY             = :files
+      TEMPLATES_KEY         = :templates
+      LINES_KEY             = :lines
+      COMMENTS_KEY          = :comments
+      MANIFESTS_KEY         = :manifests
+      LIBS_KEY              = :libs
+      MODULES_KEY           = :modules
+      CODE_KEY              = :code
+      HIERA_FILES_KEY       = :hiera_yaml_files
+      HIERA_KEYS_UNIQUE     = :hiera_keys_unique
+      HIERA_TOP_LEVEL_KEYS  = :top_level_keys
+
 
       def self.puppetfile
         data = File.readlines(PUPPETFILE)
@@ -13,8 +31,10 @@ class Onceover
 
         # inline definition barfs on keyname in variable
         stats = {}
-        stats[PUPPETFILE_KEY] = {
-          modules: mod_count
+        stats = {
+          TOTALS_KEY => {
+            MODULES_KEY => mod_count,
+          }
         }
 
         stats
@@ -33,67 +53,39 @@ class Onceover
         dirs
       end
 
-# overall code stats hash looks like this:
-# {
-#   "puppetfile": {
-#     "modules": 63
-#   },
-#   "totals": {
-#     "lines": 1070,
-#     "comments": 572,
-#     "code": 498,
-#     "manifests": 25,
-#     "templates": 1,
-#     "files": 1,
-#     "libs": 0,
-#     "modules": 2
-#   },
-#   "site_modules": {
-#     "site": {
-#       "role": {
-#         "manifests": {
-#           "trusted_fact_classified.pp": {
-#             "lines": 50,
-#             "comments": 29,
-#             "code": 21
-#           },
-#       "templates": 0,
-#         "files": 0,
-#         "libs": 0
-#       },
       def self.code_stats
         # per module+file stats
-        stats = {}
+        stats = {
+          # agregate stats
+          TOTALS_KEY => {
+            LINES_KEY => 0,
+            COMMENTS_KEY => 0,
+            CODE_KEY => 0,
+            MANIFESTS_KEY => 0,
+            TEMPLATES_KEY => 0,
+            FILES_KEY => 0,
+            LIBS_KEY => 0,
+            MODULES_KEY => 0,
+          },
 
-        # agregate stats
-        stats[TOTALS_KEY] = {
-          lines: 0,
-          comments: 0,
-          code: 0,
-          manifests: 0,
-          templates: 0,
-          files: 0,
-          libs: 0,
-          modules: 0,
+          # per module scan path stats
+          CODE_KEY => {
+            SITE_MODULES_KEY => {},
+          },
         }
-
-        stats[SITE_MODULES_KEY] = {}
 
         # each scan path
         modpath_dirs.each { |modpath_dir|
           # ..but a scan path element may not exist...
           if Dir.exist? modpath_dir
-            stats[SITE_MODULES_KEY][modpath_dir] = {}
+            stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir] = {}
 
             Dir.entries(modpath_dir).select { |mod_dir|
               File.directory? File.join(modpath_dir, mod_dir) and !(mod_dir =='.' || mod_dir == '..')
             }.each do |mod_dir|
               # each module dir
-              stats[SITE_MODULES_KEY][modpath_dir][mod_dir] = {
-                manifests: {},
-                templates: 0,
-                files: 0,
-                libs: 0,
+              stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir][mod_dir] = {
+                MANIFESTS_KEY => {},
               }
 
 
@@ -107,27 +99,30 @@ class Onceover
                     lines                = data.size
                     comments             = data.grep(/^\s*#/).size
                     code                 = lines - comments
-                    stats[SITE_MODULES_KEY][modpath_dir][mod_dir][:manifests][pp_file] = {
-                      lines: lines,
-                      comments: comments,
-                      code: code,
+                    stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir][mod_dir][MANIFESTS_KEY][pp_file] = {
+                      LINES_KEY => lines,
+                      COMMENTS_KEY => comments,
+                      CODE_KEY => code,
                     }
 
-                    stats[TOTALS_KEY][:lines] += lines
-                    stats[TOTALS_KEY][:comments] += comments
-                    stats[TOTALS_KEY][:code] += code
-                    stats[TOTALS_KEY][:manifests] += 1
+                    # update the global total counts
+                    stats[TOTALS_KEY][LINES_KEY] += lines
+                    stats[TOTALS_KEY][COMMENTS_KEY] += comments
+                    stats[TOTALS_KEY][CODE_KEY] += code
+                    stats[TOTALS_KEY][MANIFESTS_KEY] += 1
                   }
                 end
-                stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY] = {}
-                stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][:templates] = Dir["templates/**/*"].count { |file| File.file?(file) }
-                stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][:libs] = Dir["lib/**/*"].count { |file| File.file?(file) }
-                stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][:files] = Dir["files/**/*"].count { |file| File.file?(file) }
 
-                stats[TOTALS_KEY][:templates] += stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][:templates]
-                stats[TOTALS_KEY][:libs] += stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][:libs]
-                stats[TOTALS_KEY][:files] += stats[SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][:files]
-                stats[TOTALS_KEY][:modules] += 1
+                stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY] = {
+                  TEMPLATES_KEY => (Dir["templates/**/*"].count { |file| File.file?(file)}),
+                  LIBS_KEY => (Dir["lib/**/*"].count { |file| File.file?(file)}),
+                  FILES_KEY => (Dir["files/**/*"].count { |file| File.file?(file)}),
+                }
+
+                stats[TOTALS_KEY][TEMPLATES_KEY] += stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][TEMPLATES_KEY]
+                stats[TOTALS_KEY][LIBS_KEY] += stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][LIBS_KEY]
+                stats[TOTALS_KEY][FILES_KEY] += stats[CODE_KEY][SITE_MODULES_KEY][modpath_dir][mod_dir][TOTALS_KEY][FILES_KEY]
+                stats[TOTALS_KEY][MODULES_KEY] += 1
               end
             end
           end
@@ -137,21 +132,72 @@ class Onceover
   
       end
 
+      # assumes hiera data in /data
+      def self.hiera
+        stats = {
+          HIERA_KEY => {},
+          TOTALS_KEY => {},
+          HIERA_KEY => {
+            YAML_KEY => {},
+          },
+        }
+        total_lines = 0
+        yaml_files = Dir["data/**/*.yaml"]
+
+        # parse each file to count top-level keys
+        top_level_keys = []
+        yaml_files.each do |yaml_file|
+          lines = File.readlines(yaml_file).length
+
+          data = YAML.load_file(yaml_file)
+          stats[HIERA_KEY][YAML_KEY][yaml_file] = {
+            LINES_KEY => lines,
+          }
+
+          total_lines += lines
+          if data
+            # there may be files with no keys...
+            stats[HIERA_KEY][YAML_KEY][yaml_file][HIERA_TOP_LEVEL_KEYS] = data.keys.length
+            top_level_keys << data.keys
+          end
+
+        end
+
+        stats[TOTALS_KEY] = {
+          HIERA_FILES_KEY   => yaml_files.length,
+          HIERA_KEYS_UNIQUE => top_level_keys.uniq.length,
+          LINES_KEY         => total_lines,
+        }
+
+        stats
+      end
+
+
       def self.print_table(h, indent="")
         col_width = 50 - indent.length
         h.each do |k,v|
-          printf("#{indent}%-#{col_width}s %10s\n", k, v)
+          kf = k.to_s.gsub(/_/, ' ')
+          printf("#{indent}%-#{col_width}s %10s\n", kf, v)
         end
       end
 
       def self.format_report(stats, detailed)
 
         puts "----- [Puppetfile] -----"
-        print_table(stats[PUPPETFILE_KEY])
+        print_table(stats[TOTALS_KEY][PUPPETFILE_KEY])
         puts ""
 
+        if detailed
+          puts "----- [Hiera] -----"
+          stats[HIERA_KEY][YAML_KEY].each do |yaml_file, data|
+            puts yaml_file
+            print_table(data, "  ")
+          end
+          puts ""
+        end
+
         puts "----- [Puppet Code] -----"
-        stats[SITE_MODULES_KEY].each do |modpath, modpath_data|
+        stats[CODE_KEY][SITE_MODULES_KEY].each do |modpath, modpath_data|
           # each path element
           puts "#{modpath}"
 
@@ -165,9 +211,10 @@ class Onceover
             print_table(mod_data[TOTALS_KEY], indent)
 
             # per-manifest stats
-            puts "#{indent}manifests (#{mod_data[:manifests].length})"
+            print_table({MANIFESTS_KEY => mod_data[MANIFESTS_KEY].length}, indent)
+
             if detailed
-              mod_data[:manifests].each { |pp_file, stats|
+              mod_data[MANIFESTS_KEY].each { |pp_file, stats|
 
                 indent = "      "
                 puts "#{indent}#{pp_file}"
@@ -181,16 +228,46 @@ class Onceover
         puts ""
 
         puts "***** [TOTALS] *****"
-        print_table(stats[TOTALS_KEY])
+        stats[TOTALS_KEY].each do |k,v|
+          puts k
+          print_table(v, "  ")
+        end
+
 
       end
 
       def self.run(opts)
-        stats = puppetfile.merge(code_stats)
+        puppetfile = puppetfile()
+        code_stats = code_stats()
+        hiera = hiera()
+
+        stats = {
+          CODE_KEY => code_stats[CODE_KEY],
+          HIERA_KEY => hiera[HIERA_KEY],
+          TOTALS_KEY => {
+            PUPPETFILE_KEY => puppetfile[TOTALS_KEY],
+            CODE_KEY => code_stats[TOTALS_KEY],
+            HIERA_KEY => hiera[TOTALS_KEY],
+            TOTALS_KEY => {
+              LINES_KEY => code_stats[TOTALS_KEY][LINES_KEY] + hiera[TOTALS_KEY][LINES_KEY],
+              MODULES_KEY => puppetfile[TOTALS_KEY][MODULES_KEY] + code_stats[TOTALS_KEY][MODULES_KEY]
+            }
+          },
+
+        }
+
+#        stats = {}.deep_merge(puppetfile).deep_merge(code_stats).deep_merge(hiera)
 
         if opts[:format] == "json"
           pretty_json = JSON.pretty_generate(stats)
           puts pretty_json
+        elsif opts[:format] == "csv"
+          # only basic stats for CSV right now...
+          csv_string = CSV.generate do |csv|
+            stats[TOTALS_KEY].to_a.each {|elem| csv << elem}
+          end
+
+          puts csv_string
         else
           # human mode
           format_report(stats, opts[:detailed])
